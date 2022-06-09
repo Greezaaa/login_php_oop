6af524493e84e91c393c4035dedd3e36aca7cb9f
Jeka <greezaaa@gmail.com>
2022-06-09 16:36:06 +0200

    db.sql added

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS `learn_oop`;
+
+CREATE TABLE IF NOT EXISTS `learn_oop`.`loginUsers` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(250) NOT NULL,
+    `email` VARCHAR(250) NOT NULL,
+    `password` VARCHAR(250) NOT NULL,
+    `data_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
